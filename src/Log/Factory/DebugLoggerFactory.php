26c516dd81ede9e1a33397f23b6fc9563e19c52c
Andreas Frömer <andreas.froemer@check24.de>
2019-04-11 16:16:21 +0200

    Use NullLogger when not in debug mode
    Reference #41

--- src/Log/Factory/DebugLoggerFactory.php
+++ src/Log/Factory/DebugLoggerFactory.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace Icanhazstring\Composer\Unused\Log\Factory;
 
+use Composer\IO\IOInterface;
 use Icanhazstring\Composer\Unused\Log\DebugLogger;
 use Icanhazstring\Composer\Unused\Log\LogHandlerInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class DebugLoggerFactory
 {
     public function __invoke(ContainerInterface $container): LoggerInterface
     {
+        /** @var IOInterface $io */
+        $io = $container->get(IOInterface::class);
+
+        if ($io->isDebug() === false) {
+            return new NullLogger();
+        }
+
         return new DebugLogger($container->get(LogHandlerInterface::class));
     }
 }
