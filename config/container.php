57c4c6e30510689390ab97e5409928d04e501485
Andreas Frömer <icanhazstring@users.noreply.github.com>
2019-10-24 16:20:13 +0200

    Fix issue with old servicemanager

--- config/container.php
+++ config/container.php
@@ -2,4 +2,4 @@
 
 declare(strict_types=1);
 
-return new \Zend\ServiceManager\ServiceManager(require __DIR__ . '/service_manager.php');
+return new \Icanhazstring\Composer\Unused\Di\ServiceContainer(require __DIR__ . '/service_manager.php');
