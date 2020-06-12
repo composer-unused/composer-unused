72a9502b8461717f42999eecc02baf0776421ae0
Andreas Frömer <andreas.froemer@check24.de>
2020-06-12 11:50:29 +0200

    Add NamespaceSymbol for PSR-0/4 autoload configurations
    This is to avoid a full scan of the complete package
    structure which is not necessary. It represents the same
    logic as before with composer-unused.
    
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

--- src/Symbol/Symbol.php
+++ src/Symbol/Symbol.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace Icanhazstring\Composer\Unused\Symbol;
 
-final class Symbol
+final class Symbol implements SymbolInterface
 {
-    private $name;
+    private $identifier;
 
-    public function __construct(string $name)
+    public function __construct(string $identifier)
     {
-        $this->name = $name;
+        $this->identifier = $identifier;
     }
 
-    public function matches(Symbol $symbol): bool
+    public function getIdentifier(): string
     {
-        return $this->name === $symbol->name;
+        return $this->identifier;
+    }
+
+
+    public function matches(SymbolInterface $symbol): bool
+    {
+        return $this->identifier === $symbol->getIdentifier();
     }
 }
