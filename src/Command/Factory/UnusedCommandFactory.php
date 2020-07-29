73fa45e28a59bdb6fb2119715cbb893889b1ddba
Andreas Frömer <andreas.froemer@check24.de>
2020-07-29 23:40:29 +0200

    Extract dependencies
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

--- src/Command/Factory/UnusedCommandFactory.php
+++ src/Command/Factory/UnusedCommandFactory.php
@@ -8,6 +8,10 @@ use Icanhazstring\Composer\Unused\Command\UnusedCommand;
 use Icanhazstring\Composer\Unused\Error\ErrorHandlerInterface;
 use Icanhazstring\Composer\Unused\Loader\LoaderBuilder;
 use Icanhazstring\Composer\Unused\Output\SymfonyStyleFactory;
+use Icanhazstring\Composer\Unused\Symbol\Loader\CompositeSymbolLoader;
+use Icanhazstring\Composer\Unused\Symbol\Loader\ExtensionSymbolLoader;
+use Icanhazstring\Composer\Unused\Symbol\Loader\FileSymbolLoader;
+use Icanhazstring\Composer\Unused\Symbol\Loader\PsrSymbolLoader;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 
@@ -19,7 +23,14 @@ class UnusedCommandFactory
             $container->get(ErrorHandlerInterface::class),
             new SymfonyStyleFactory(),
             $container->get(LoaderBuilder::class),
-            $container->get(LoggerInterface::class)
+            $container->get(LoggerInterface::class),
+            new CompositeSymbolLoader(
+                [
+                    $container->get(ExtensionSymbolLoader::class),
+                    $container->get(PsrSymbolLoader::class),
+                    $container->get(FileSymbolLoader::class)
+                ]
+            )
         );
     }
 }
