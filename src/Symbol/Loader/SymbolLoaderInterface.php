9201bc96ec22132302dbcd40e849ecaaea8b7d49
Andreas Frömer <andreas.froemer@check24.de>
2020-08-01 00:00:28 +0200

    Add PackageDecorator
    The package decorator is needed to actually locate source
    files given from autoload directives. This is needed as
    packages don't have information about their specific file
    system location.
    
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

--- src/Symbol/Loader/SymbolLoaderInterface.php
+++ src/Symbol/Loader/SymbolLoaderInterface.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Icanhazstring\Composer\Unused\Symbol\Loader;
 
-use Composer\Package\PackageInterface;
 use Generator;
+use Icanhazstring\Composer\Unused\Composer\PackageDecoratorInterface;
 use Icanhazstring\Composer\Unused\Symbol\SymbolInterface;
 
 interface SymbolLoaderInterface
 {
     /**
+     * @param PackageDecoratorInterface $package
      * @return Generator<SymbolInterface>
      */
-    public function load(PackageInterface $package): Generator;
+    public function load(PackageDecoratorInterface $package): Generator;
 }
