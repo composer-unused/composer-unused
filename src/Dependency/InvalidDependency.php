158260725f2bb3e6c51284430a7a13f79af2db44
Andreas Frömer <andreas.froemer@check24.de>
2020-09-15 16:31:49 +0200

    Add dependency collection logic
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

---
+++ src/Dependency/InvalidDependency.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Icanhazstring\Composer\Unused\Dependency;
+
+use Composer\Package\Link;
+use Icanhazstring\Composer\Unused\Symbol\SymbolInterface;
+
+final class InvalidDependency implements DependencyInterface
+{
+    /** @var Link */
+    private $linkedPackage;
+    /** @var string */
+    private $reason;
+
+    public function __construct(Link $linkedPackage, string $reason)
+    {
+        $this->linkedPackage = $linkedPackage;
+        $this->reason = $reason;
+    }
+
+    public function getName(): string
+    {
+        return $this->linkedPackage->getTarget();
+    }
+
+    public function getReason(): string
+    {
+        return $this->reason;
+    }
+
+    public function inState(string $state): bool
+    {
+        return self::STATE_INVALID === $state;
+    }
+
+    public function provides(SymbolInterface $symbol): bool
+    {
+        return false;
+    }
+}
