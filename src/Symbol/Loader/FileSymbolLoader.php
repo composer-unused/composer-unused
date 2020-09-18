d1311500c492b3a51037b44d955a6728ceeb61aa
Andreas Frömer <andreas.froemer@check24.de>
2020-09-18 23:15:45 +0200

    Fix issue with FileSymbolLoader not using classmap paths
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

--- src/Symbol/Loader/FileSymbolLoader.php
+++ src/Symbol/Loader/FileSymbolLoader.php
@@ -8,8 +8,10 @@ use Generator;
 use Icanhazstring\Composer\Unused\Composer\PackageDecoratorInterface;
 use Icanhazstring\Composer\Unused\Exception\IOException;
 use Icanhazstring\Composer\Unused\Symbol\Provider\FileSymbolProvider;
-use SplFileInfo;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
+use function array_map;
 use function sprintf;
 
 class FileSymbolLoader implements SymbolLoaderInterface
@@ -27,19 +29,40 @@ class FileSymbolLoader implements SymbolLoaderInterface
      */
     public function load(PackageDecoratorInterface $package): Generator
     {
-        $files = [];
+        $classmapPaths = $this->resolvePackageSourcePath(
+            $package,
+            $package->getAutoload()['classmap'] ?? []
+        );
+        $filePaths = $this->resolvePackageSourcePath(
+            $package,
+            $package->getAutoload()['files'] ?? []
+        );
 
-        foreach ($package->getAutoload()['files'] ?? [] as $path) {
-            $files[] = new SplFileInfo(
-                sprintf(
-                    '%s/vendor/%s/%s',
-                    $package->getBaseDir(),
-                    $package->getName(),
-                    $path
-                )
-            );
-        }
+        $finder = new Finder();
+
+        /** @var SplFileInfo[]|Finder $files */
+        $files = $finder
+            ->files()
+            ->name('*.php')
+            ->in($classmapPaths)
+            ->append($filePaths);
 
         yield from $this->fileSymbolProvider->provide($package->getTargetDir(), $files);
+    }
+
+    /**
+     * @param array<string> $paths
+     * @return array<string>
+     */
+    private function resolvePackageSourcePath(PackageDecoratorInterface $package, array $paths): array
+    {
+        return array_map(static function (string $path) use ($package) {
+            return sprintf(
+                '%s/vendor/%s/%s',
+                $package->getBaseDir(),
+                $package->getName(),
+                $path
+            );
+        }, $paths);
     }
 }
