0e37ea6196b676c8090798bb2a0f5db27bdbc459
Verena Röösli <vr@rikt.ch>
2021-10-21 09:38:23 +0200

    Added pattern exclusion support for "-implementation" packages

--- src/Command/FilterDependencyCollectionCommand.php
+++ src/Command/FilterDependencyCollectionCommand.php
@@ -10,23 +10,34 @@ use function array_merge;
 
 final class FilterDependencyCollectionCommand
 {
-    private const GLOBAL_EXCLUSION = [
-        'composer-plugin-api',
+    private const GLOBAL_NAMED_EXCLUSION = [
+        'composer-plugin-api'
+    ];
+
+    private const GLOBAL_PATTERN_EXCLUSION = [
+        '/-implementation$/i'
     ];
 
     /** @var DependencyCollection */
     private $requiredDependencyCollection;
     /** @var array<string> */
     private $namedExclusion;
+    /** @var array<string> */
+    private $patternExclusion;
 
     /**
      * @param DependencyCollection $requiredDependencyCollection
      * @param array<string> $namedExclusion
+     * @param array<string> $patternExclusion
      */
-    public function __construct(DependencyCollection $requiredDependencyCollection, array $namedExclusion)
-    {
+    public function __construct(
+        DependencyCollection $requiredDependencyCollection,
+        array $namedExclusion = [],
+        array $patternExclusion = []
+    ) {
         $this->requiredDependencyCollection = $requiredDependencyCollection;
-        $this->namedExclusion = array_merge(self::GLOBAL_EXCLUSION, $namedExclusion);
+        $this->namedExclusion = array_merge(self::GLOBAL_NAMED_EXCLUSION, $namedExclusion);
+        $this->patternExclusion = array_merge(self::GLOBAL_PATTERN_EXCLUSION, $patternExclusion);
     }
 
     public function getRequiredDependencyCollection(): DependencyCollection
@@ -40,5 +51,13 @@ final class FilterDependencyCollectionCommand
     public function getNamedExclusion(): array
     {
         return $this->namedExclusion;
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getPatternExclusion(): array
+    {
+        return $this->patternExclusion;
     }
 }
