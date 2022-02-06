fc96887c37cf78951336e58a34f7cfb456c05f16
Andreas Frömer <andreas.froemer@check24.de>
2022-02-06 13:51:40 +0100

    Use CIDetector to change output automatically
    Signed-off-by: Andreas Frömer <andreas.froemer@check24.de>

--- src/OutputFormatter/FormatterFactory.php
+++ src/OutputFormatter/FormatterFactory.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace ComposerUnused\ComposerUnused\OutputFormatter;
 
+use OndraM\CiDetector\CiDetector;
+use OndraM\CiDetector\Exception\CiNotDetectedException;
+
 final class FormatterFactory
 {
-    public static function create(string $type): OutputFormatterInterface
+    public static function create(?string $type): OutputFormatterInterface
     {
+        $ciDetector = new CiDetector();
+
+        try {
+            $ci = $ciDetector->detect();
+            if ($ci->getCiName() === CiDetector::CI_GITHUB_ACTIONS) {
+                $type = 'github';
+            }
+        } catch (CiNotDetectedException $exception) {
+            $type = 'default';
+        }
+
         switch ($type) {
             case 'github':
                 return new GithubFormatter();
