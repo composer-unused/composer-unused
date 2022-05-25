4bb8dc88bb96386e38f9e31c68e8790025056786
Alessandro Lai <alessandro.lai85@gmail.com>
2022-05-25 11:16:36 +0200

    Fix polyfill CS

--- config/polyfill_symfony_di.php
+++ config/polyfill_symfony_di.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-assert(class_exists(ContainerConfigurator::class, true));
-
-if (! function_exists('\Symfony\Component\DependencyInjection\Loader\Configurator\service')) {
+if (class_exists(ContainerConfigurator::class, true) && ! function_exists('\Symfony\Component\DependencyInjection\Loader\Configurator\service')) {
     function service(string $serviceId): ReferenceConfigurator
     {
         return ref($serviceId);
