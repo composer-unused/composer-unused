45355927c056d4d2ff8de6366e9293022b2366c1
Oskar Stark <oskarstark@googlemail.com>
2022-06-28 09:22:45 +0200

    Add missing use statement

--- composer-unused.php
+++ composer-unused.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
 use Webmozart\Glob\Glob;
 
