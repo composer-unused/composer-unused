b54274c9148985cce4bef8a71f075ade405d2432
Tomas Votruba <tomas.vot@gmail.com>
2022-10-04 11:02:58 +0200

    Add JsonFormatter with test

---
+++ src/OutputFormatter/JsonFormatter.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerUnused\ComposerUnused\OutputFormatter;
+
+use ComposerUnused\ComposerUnused\Dependency\DependencyCollection;
+use ComposerUnused\ComposerUnused\Dependency\DependencyInterface;
+use ComposerUnused\ComposerUnused\Filter\FilterCollection;
+use ComposerUnused\Contracts\PackageInterface;
+use Symfony\Component\Console\Style\OutputStyle;
+
+final class JsonFormatter implements OutputFormatterInterface
+{
+    public function formatOutput(
+        PackageInterface $rootPackage,
+        string $composerJsonPath,
+        DependencyCollection $usedDependencyCollection,
+        DependencyCollection $unusedDependencyCollection,
+        DependencyCollection $ignoredDependencyCollection,
+        FilterCollection $filterCollection,
+        OutputStyle $output
+    ): int {
+        $jsonResult = [];
+
+        foreach ($usedDependencyCollection as $usedDependency) {
+            $packageJson = [];
+
+            if (!empty($usedDependency->getRequiredBy())) {
+                $requiredByNames = array_map(static function (DependencyInterface $dependency) {
+                    return $dependency->getName();
+                }, $usedDependency->getRequiredBy());
+
+                $packageJson['required-by'] = $requiredByNames;
+            }
+
+            if (!empty($usedDependency->getSuggestedBy())) {
+                $suggestedByNames = array_map(static function (DependencyInterface $dependency) {
+                    return $dependency->getName();
+                }, $usedDependency->getSuggestedBy());
+
+                $packageJson['suggested-by'] = $suggestedByNames;
+            }
+
+            $packageJson['name'] = $usedDependency->getName();
+
+            $jsonResult['used-packages'][] = $packageJson;
+        }
+
+        foreach ($unusedDependencyCollection as $dependency) {
+            $jsonResult['unused-packages'][] = $dependency->getName();
+        }
+
+        foreach ($ignoredDependencyCollection as $dependency) {
+            $jsonResult['ignored-packages'][] = $dependency->getName();
+        }
+
+        foreach ($filterCollection->getUnused() as $filter) {
+            $jsonResult['zombie-exclusions'][] = $filter->toString();
+        }
+
+        $json = json_encode($jsonResult, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
+        $output->write($json);
+
+        if ($unusedDependencyCollection->count() > 0 || count($filterCollection->getUnused()) > 0) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
---
+++ tests/Unit/OutputFormatter/JsonFormatter/JsonFormatterTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerUnused\ComposerUnused\Test\Unit\OutputFormatter\JsonFormatter;
+
+use ComposerUnused\ComposerUnused\Composer\Package;
+use ComposerUnused\ComposerUnused\Dependency\DependencyCollection;
+use ComposerUnused\ComposerUnused\Dependency\RequiredDependency;
+use ComposerUnused\ComposerUnused\Filter\FilterCollection;
+use ComposerUnused\ComposerUnused\OutputFormatter\JsonFormatter;
+use ComposerUnused\Contracts\PackageInterface;
+use PHPStan\Testing\TestCase;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class JsonFormatterTest extends TestCase
+{
+    private JsonFormatter $jsonFormatter;
+
+    protected function setUp(): void
+    {
+        $this->jsonFormatter = new JsonFormatter();
+    }
+
+
+    /**
+     * @test
+     */
+    public function itPrints(): void
+    {
+        $usedDependencyCollection = new DependencyCollection([
+            new RequiredDependency(new Package('symfony/string'))
+        ]);
+
+        $unusedDependencyCollection = new DependencyCollection([
+            new RequiredDependency(new Package('symfony/console'))
+        ]);
+
+        $bufferedOutput = new BufferedOutput();
+        $outputStyle = new SymfonyStyle(new ArgvInput(), $bufferedOutput);
+
+        $this->jsonFormatter->formatOutput(
+            $this->createMock(PackageInterface::class),
+            'composer.json',
+            $usedDependencyCollection,
+            $unusedDependencyCollection,
+            $this->createMock(DependencyCollection::class),
+            $this->createMock(FilterCollection::class),
+            $outputStyle
+        );
+
+        $consoleOutput = $bufferedOutput->fetch() . PHP_EOL;
+        self::assertStringEqualsFile(__DIR__ . '/Fixture/expected_unused_packages.json', $consoleOutput);
+    }
+}
