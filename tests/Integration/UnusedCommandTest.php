5c968d72ab48e8e41201c8ae454a127b77b8f392
Pascal <pascal@heidmann.io>
2022-10-05 22:16:32 +0200

    Add testcase for readonly class

--- tests/Integration/UnusedCommandTest.php
+++ tests/Integration/UnusedCommandTest.php
@@ -222,6 +222,21 @@ class UnusedCommandTest extends TestCase
     /**
      * @test
      */
+    public function itShouldRunWithReadonlyClassInDependency(): void
+    {
+        $commandTester = new CommandTester(self::$container->get(UnusedCommand::class));
+        $exitCode = $commandTester->execute(['composer-json' => __DIR__ . '/../assets/TestProjects/DependencyWithReadonlyClass/composer.json']);
+
+        self::assertStringContainsString(
+            'Found 1 used, 0 unused, 0 ignored and 0 zombie packages',
+            $commandTester->getDisplay()
+        );
+        self::assertSame(0, $exitCode);
+    }
+
+    /**
+     * @test
+     */
     public function itShouldHaveZeroExitCodeWithIgnoreExitCodeOptionOnError(): void
     {
         $commandTester = new CommandTester(self::$container->get(UnusedCommand::class));
