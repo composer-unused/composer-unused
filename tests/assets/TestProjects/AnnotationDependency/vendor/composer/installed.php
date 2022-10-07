0a90c165aead4c565b298bfbc82143bfff263faa
Leo Viezens <leo.viezens@web.de>
2022-10-07 15:11:54 +0200

    #94 Don't report packages used in annotations

---
+++ tests/assets/TestProjects/AnnotationDependency/vendor/composer/installed.php
@@ -0,0 +1,17 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'ns/lib',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'first/dependency' => array(
+            'install_path' => __DIR__ . '/../first/dependency',
+        ),
+    ),
+);
