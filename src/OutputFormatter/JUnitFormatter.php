fc5eadbf384e5dee86b81cb92144ffa9e9537e1c
martin.reinfandt <martin.reinfandt@check24.de>
2023-03-15 14:48:55 +0100

    adjust output of junit formatter

--- src/OutputFormatter/JUnitFormatter.php
+++ src/OutputFormatter/JUnitFormatter.php
@@ -23,36 +23,38 @@ class JUnitFormatter implements OutputFormatterInterface
         $xml = '<?xml version="1.0" encoding="UTF-8"?>';
 
         $totalFailuresCount = count($unusedDependencyCollection) + count($filterCollection->getUnused());
-        $totalTestsCount = $totalFailuresCount + count($ignoredDependencyCollection);
+        $totalSkippedCount = count($ignoredDependencyCollection);
+        $totalTestsCount = $totalFailuresCount + $totalSkippedCount;
 
         $xml .= sprintf(
-            '<testsuite failures="%d" name="composer-unused" tests="%d" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">',
+            '<testsuite name="composer-unused" failures="%d" skipped="%d" tests="%d" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">',
             $totalFailuresCount,
+            $totalSkippedCount,
             $totalTestsCount
         );
 
         if (count($unusedDependencyCollection) > 0) {
-            $xml .= '<testcase name="unused-dependencies">';
             foreach ($unusedDependencyCollection as $dependency) {
-                $xml .= sprintf('<failure type="ERROR" message="%s" />', $this->escape($dependency->getName()));
+                $xml .= sprintf('<testcase name="%s">', $this->escape($dependency->getName()));
+                $xml .= '<failure type="ERROR" message="Dependency is unused" />';
+                $xml .= '</testcase>';
             }
-            $xml .= '</testcase>';
         }
 
         if (count($ignoredDependencyCollection) > 0) {
-            $xml .= '<testcase name="ignored-dependencies">';
             foreach ($ignoredDependencyCollection as $dependency) {
-                $xml .= sprintf('<failure type="WARNING" message="%s" />', $this->escape($dependency->getName()));
+                $xml .= sprintf('<testcase name="%s">', $this->escape($dependency->getName()));
+                $xml .= '<skipped message="Dependency is ignored" />';
+                $xml .= '</testcase>';
             }
-            $xml .= '</testcase>';
         }
 
         if (count($filterCollection->getUnused()) > 0) {
-            $xml .= '<testcase name="zombie-exclusions">';
             foreach ($filterCollection->getUnused() as $filter) {
-                $xml .= sprintf('<failure type="ERROR" message="%s" />', $this->escape($filter->toString()));
+                $xml .= sprintf('<testcase name="%s">', $this->escape($filter->toString()));
+                $xml .= '<failure type="ERROR" message="Dependency did not match any package" />';
+                $xml .= '</testcase>';
             }
-            $xml .= '</testcase>';
         }
 
         $xml .= '</testsuite>';
