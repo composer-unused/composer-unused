5b8b434fe169ac65b8ff8f458f7e685aa18b3f04
MarcinGladkowski <marcingladkowski@gmail.com>
2023-06-18 08:46:53 +0200

    Issue_534: check installed.json structure for key added in Composer 2

---
+++ src/Composer/Exception/InvalidComposerVersionInstalledPackages.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerUnused\ComposerUnused\Composer\Exception;
+
+use RuntimeException;
+
+class InvalidComposerVersionInstalledPackages extends RuntimeException
+{
+}
