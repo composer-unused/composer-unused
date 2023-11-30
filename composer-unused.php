96840b0e046c3952ec1df348242d21fb5572dfde
Andreas Frömer <andreas.froemer@finanztip.de>
2023-11-30 14:03:42 +0100

    Update vendor-bin dependencies

--- composer-unused.php
+++ composer-unused.php
@@ -8,11 +8,18 @@ use ComposerUnused\ComposerUnused\Configuration\PatternFilter;
 use Webmozart\Glob\Glob;
 
 return static function (Configuration $config): Configuration {
-    return $config
-        // ->addNamedFilter(NamedFilter::fromString('symfony/config'))
+    $config
+        // ->addNamedFilter(NamedFilter::fromString('symfony/property-access'))
         // ->addPatternFilter(PatternFilter::fromString('/symfony\/.*/'))
         ->setAdditionalFilesFor('icanhazstring/composer-unused', [
             __FILE__,
             ...Glob::glob(__DIR__ . '/config/*.php'),
         ]);
+
+    // symfony/serializer with php8.1 installs a version that is no longer suggesting property-access
+    if (PHP_VERSION_ID >= 80100) {
+        $config->addNamedFilter(NamedFilter::fromString('symfony/property-access'));
+    }
+
+    return $config;
 };
