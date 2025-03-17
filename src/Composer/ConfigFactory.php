aa7aac930e02d08d975053ce0c66405a5787a77f
Andreas Frömer <andreas.froemer@finanztip.de>
2025-03-17 11:54:39 +0100

    fix: Allow __root__ as default package name.

--- src/Composer/ConfigFactory.php
+++ src/Composer/ConfigFactory.php
@@ -4,21 +4,16 @@ declare(strict_types=1);
 
 namespace ComposerUnused\ComposerUnused\Composer;
 
-use ComposerUnused\ComposerUnused\Composer\Validator\ValidatorFactory;
-use InvalidArgumentException;
 use RuntimeException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ConfigFactory
 {
     private SerializerInterface $serializer;
-
-    private ValidatorInterface $validator;
 
     public function __construct()
     {
@@ -26,8 +21,6 @@ final class ConfigFactory
         $normalizers = [new PropertyNormalizer(null, new CamelCaseToSnakeCaseNameConverter())];
 
         $this->serializer = new Serializer($normalizers, $encoders);
-
-        $this->validator = ValidatorFactory::createValidator();
     }
 
     public function fromPath(string $jsonPath): Config
@@ -43,23 +36,6 @@ final class ConfigFactory
         $config->setRaw($composerJson);
         $config->setBaseDir(dirname($jsonPath));
 
-        $this->validate($config);
-
         return $config;
-    }
-
-    private function validate(Config $config): void
-    {
-        $violations = $this->validator->validate($config);
-
-        if (count($violations) !== 0) {
-            $message = 'Validation errors:';
-
-            foreach ($violations as $violation) {
-                $message .=  ' ' . $violation->getMessage();
-            }
-
-            throw new InvalidArgumentException($message);
-        }
     }
 }
